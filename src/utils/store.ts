07106ea8f54fb3aadae2bb25481c8c3fa09486a0
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-12-14 21:59:14 +0900

    refactor: seperate input class

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,53 +1,5 @@
-import { ParamsValueType, TFormParamsType } from "../hooks/form/useTForm";
-
-type ErrorType = {
-  state: boolean;
-  message: string[];
-};
-
-type InputParamsType = Omit<ParamsValueType, "type" | "onChnageModeㄹ">;
-
-export class Input {
-  private value?: string = "";
-  private placeholder?: string = "";
-  private error: ErrorType = { state: false, message: [""] };
-
-  isValid: boolean = false;
-  isDirty: boolean = false;
-  isTouched: boolean = false;
-
-  constructor(props: InputParamsType) {
-    this.value = props.defaultValue;
-    this.placeholder = props.placeholder;
-  }
-
-  setInputValue(value: string) {
-    this.value = value;
-  }
-
-  setInputError(state?: boolean, message?: string[]) {
-    this.error.state = state ?? this.error.state;
-    this.error.message = message
-      ? [...this.error.message, ...message]
-      : this.error.message;
-  }
-
-  getValue() {
-    return this.value;
-  }
-
-  getPlaceholder() {
-    return this.placeholder;
-  }
-
-  getError() {
-    return this.error;
-  }
-
-  static setValue(target: Input, currentValue: string) {
-    target.value = currentValue;
-  }
-}
+import { TFormParamsType } from "../types/common";
+import { Input } from "./input";
 
 type StoreType = {
   [Name in keyof TFormParamsType]: Input;
