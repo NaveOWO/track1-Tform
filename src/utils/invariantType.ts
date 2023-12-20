45b715734c3da502a3126ffe315c9f8c0ed0b76e
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-12-20 15:46:06 +0900

    feat: invariantType 생성

---
+++ src/utils/invariantType.ts
@@ -0,0 +1,27 @@
+declare const tag: unique symbol;
+
+declare type InvariantProperty<T> = (arg: T) => T;
+
+declare type InvariantSignature<T> = {
+  readonly [tag]: InvariantProperty<T>;
+};
+
+export type InvariantOf<T> = T & InvariantSignature<T>;
+
+export function invariantOf<T>(value: T): InvariantOf<T> {
+  return value as InvariantOf<T>;
+}
+
+export function getInvariantObjectKeys<T>(arg: InvariantOf<T>): (keyof T)[] {
+  return Object.keys(arg) as unknown as (keyof T)[];
+}
+
+export function getInvariantObjectValues<T>(arg: InvariantOf<T>): T[keyof T][] {
+  return Object.keys(arg) as unknown as T[keyof T][];
+}
+
+export function getInvariantObjectEntries<T>(
+  arg: InvariantOf<T>
+): [keyof T, T[keyof T]][] {
+  return Object.entries(arg) as unknown as [keyof T, T[keyof T]][];
+}
